fdd5f07b646af0d50f53e32bf86bb6eb1c8d0948
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2025-11-07 16:12:45 +0100

    Resize canvas to full size

--- app/Editor.tsx
+++ app/Editor.tsx
@@ -46,66 +46,65 @@ export function Editor(p: Project) {
   };
   return (
     <>
-      
+
       <div className="bg-sky-700 fixed top-0 w-full h-[50px] indent-1.5em border-b-[.1em] border-solid border-sky-800"></div>
 
       <div id="side-spec-bar" className="fixed top-[50px] right-0 w-[30%] h-(--h-spec-cont) indent-[1.5em] border-sky-800 border-solid border-t-[.1em] border-l-[.1em]">
-          <div className="bg-sky-700 h-[20px] indent-0">
-              <div id="close-button" className="fixed right-0 cursor-pointer h-[20px] text-[.85em] pr-[5px] pl-[6px] hover:bg-red-500/80">&#128473</div>
-          </div>
-
-          {tool.panel(toolCtx)}
+        <div className="bg-sky-700 h-[20px] indent-0">
+          <div id="close-button" className="fixed right-0 cursor-pointer h-[20px] text-[.85em] pr-[5px] pl-[6px] hover:bg-red-500/80">&#128473</div>
         </div>
 
+        {tool.panel(toolCtx)}
+      </div>
+
       <div id="left-side-bar" className="fixed bottom-0 left-0 w-[35.3%] h-[150px] indent-[1,5em] border-solid border-t-[.1em] border-r-[.1em] border-sky-800">
-          <div className="h-[20%] bg-sky-700"></div>
-          <div className="h-[80%] bg-zinc-900">
-              <div className="w-full h-screen"></div>
-          </div>
+        <div className="h-[20%] bg-sky-700"></div>
+        <div className="h-[80%] bg-zinc-900">
+          <div className="w-full h-screen"></div>
+        </div>
       </div>
 
       <div id="right-side-bar" className="fixed bottom-0 right-0 w-[35.3%] h-[150px] indent-[1,5em] border-solid border-t-[.1em] border-l-[.1em] border-sky-800">
-          <div className="h-[20%] bg-sky-700"></div>
-          <div className="h-[80%] bg-zinc-900"></div>
+        <div className="h-[20%] bg-sky-700"></div>
+        <div className="h-[80%] bg-zinc-900"></div>
       </div>
 
       <div className="fixed bottom-0 left-[35.3%] w-[29.4%] h-[90px] indent-[1,5em] z-0 border-solid border-sky-800 border-t-[.1em]">
-          <div className="h-[20%] bg-sky-700"></div>
-          <div className="h-[80%] bg-zinc-900">
-              <select onChange={ev => {
-              setTool(toolFromToolName[ev.target.value].make(toolCtx))
-            }
-            }>
-              {Object.values(TOOLS).map(it => it.toolname).map(it => (<option value={it} key={it}>{it}</option>))}
-            </select >
-          </div>
+        <div className="h-[20%] bg-sky-700"></div>
+        <div className="h-[80%] bg-zinc-900">
+          <select onChange={ev => {
+            setTool(toolFromToolName[ev.target.value].make(toolCtx))
+          }
+          }>
+            {Object.values(TOOLS).map(it => it.toolname).map(it => (<option value={it} key={it}>{it}</option>))}
+          </select >
+        </div>
       </div>
 
-        <svg
-          onClick={toEventHandler(tool, "click")}
-          onDoubleClick={toEventHandler(tool, "doubleclick")}
-          onMouseUp={toEventHandler(tool, "mouseup")}
-          onMouseDown={toEventHandler(tool, "mousedown")}
-          onMouseMove={toEventHandler(tool, "mousemove")}
-          style={{ width: 500, height: 500, zIndex:-1}}
-          className="bg-gray-800"
-          viewBox={
-            Object.values(project.viewBoxPos)
-              .concat([500 / project.viewBoxZoom, 500 / project.viewBoxZoom])
-              .join(" ")
-          }
-          ref={svg => {
-            svgCanvas.current = svg;
-            pt = svgCanvas.current?.createSVGPoint();
-          }
-          }
-        >
-          <defs>
-            {Object.values(ICONS)}
-          </defs>
-          {Object.values(project.devices).map((d) => DeviceComponent(d, tool))}
-          {tool.svgElements(toolCtx)}
-        </svg>
+      <svg
+        onClick={toEventHandler(tool, "click")}
+        onDoubleClick={toEventHandler(tool, "doubleclick")}
+        onMouseUp={toEventHandler(tool, "mouseup")}
+        onMouseDown={toEventHandler(tool, "mousedown")}
+        onMouseMove={toEventHandler(tool, "mousemove")}
+        className="bg-gray-800 -z-1 w-full h-full"
+        viewBox={
+          Object.values(project.viewBoxPos)
+            .concat([500 / project.viewBoxZoom, 500 / project.viewBoxZoom])
+            .join(" ")
+        }
+        ref={svg => {
+          svgCanvas.current = svg;
+          pt = svgCanvas.current?.createSVGPoint();
+        }
+        }
+      >
+        <defs>
+          {Object.values(ICONS)}
+        </defs>
+        {Object.values(project.devices).map((d) => DeviceComponent(d, tool))}
+        {tool.svgElements(toolCtx)}
+      </svg>
 
     </>
   );
