6632de73fee00dd396f564650c26d9db4906527c
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2025-11-11 22:56:52 +0100

    add devices labels

--- app/devices/deviceTypesDB.tsx
+++ app/devices/deviceTypesDB.tsx
@@ -17,12 +17,26 @@ export type DeviceType = keyof typeof deviceTypesDB
 export const DeviceComponent = memo((
   { device, extraClass }: { device: Device, extraClass?: string },
 ) => {
+  const dataProps = { "data-id": device.id };
   return (
-    <use
-      href={deviceTypesDB[device.deviceType].iconId}
-      className={"device " + (extraClass || "")}
-      {...device.pos}
-      {...{ "data-id": device.id }}
-    />
+    <>
+      <use
+        href={deviceTypesDB[device.deviceType].iconId}
+        className={"device " + (extraClass || "")}
+        {...device.pos}
+        {...dataProps}
+      />
+      <text
+        x={device.pos.x}
+        y={device.pos.y + 40}
+        width="100px"
+        textAnchor="middle"
+        fill="#ffffff"
+        className="select-none"
+        {...dataProps}
+      >
+        {device.name}
+      </text>
+    </>
   );
 })
