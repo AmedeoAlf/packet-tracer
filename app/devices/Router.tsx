0f33b3a5da4b60196c5b3027bfe9e0f6fcc3a4a9
amedeo <amedeo.alfonsi@yahoo.com>
2025-11-16 15:47:44 +0100

    Added serials to router, changed default project

--- app/devices/Router.tsx
+++ app/devices/Router.tsx
@@ -11,6 +11,8 @@ export const Router: DeviceFactory = {
       netInterfaces: [
         { name: "if0", maxMbps: 100, type: "copper" },
         { name: "if1", maxMbps: 100, type: "copper" },
+        { name: "se0", maxMbps: 100, type: "serial" },
+        { name: "se1", maxMbps: 100, type: "serial" },
       ]
     }
   }
--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,21 @@ import { Project } from "./Project";
 export default function Home() {
   const proj = useMemo(() => {
     const p = new Project();
-    p.createDevice("router", { x: 100, y: 100 })
-    p.createDevice("router", { x: 50, y: 300 })
-    p.createDevice("switch", { x: 150, y: 350 })
-    p.createDevice("switch", { x: 250, y: 350 })
-    p.createDevice("router", { x: 50, y: 50 })
-    p.createDevice("router", { x: 200, y: 50 })
-    console.assert(p.connect(1, 0, 2, 0) != undefined)
-    console.assert(p.connect(3, 0, 2, 1) != undefined)
-    console.assert(p.connect(3, 1, 4, 0) != undefined)
-    console.assert(p.connect(3, 2, 5, 0) != undefined)
-    console.assert(p.connect(3, 3, 6, 0) != undefined)
+    p.createDevice("switch", { x: 100, y: 300 }, "Rete A")
+    p.createDevice("router", { x: 250, y: 300 }, "Router A")
+    p.createDevice("router", { x: 350, y: 200 }, "Internet A")
+    p.createDevice("router", { x: 350, y: 300 }, "Internet B")
+    p.devices.get(p.lastId)?.internalState.netInterfaces.push({ name: "se2", maxMbps: 100, type: "serial" })
+    p.createDevice("router", { x: 350, y: 400 }, "Internet C")
+    p.createDevice("router", { x: 450, y: 400 }, "Router B")
+    p.createDevice("switch", { x: 600, y: 300 }, "Rete B")
+    console.assert(p.connect(1, 0, 2, 0) != undefined) // "Rete A" -> "Router A"
+    console.assert(p.connect(2, 2, 3, 2) != undefined) // "Router A" -> "Internet A"
+    console.assert(p.connect(2, 3, 4, 2) != undefined) // "Router A" -> "Internet B"
+    console.assert(p.connect(3, 3, 4, 3) != undefined) // "Internet A" -> "Internet B"
+    console.assert(p.connect(4, 4, 5, 2) != undefined) // "Internet B" -> "Internet C"
+    console.assert(p.connect(5, 3, 6, 2) != undefined) // "Internet C" -> "Router B"
+    console.assert(p.connect(6, 0, 7, 0) != undefined) // "Router B" -> "Rete B"
     return p;
   }, [0]);
   return Editor(proj);
