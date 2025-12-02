347c0b0d2b4d0eec12bf83765faca83f88d868ab
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2025-12-02 21:41:23 +0100

    Fix missing comma

--- app/page.tsx
+++ app/page.tsx
@@ -23,14 +23,12 @@ export default function Home() {
     };
 
     p.createDevice("router", { x: -50, y: -100 }, "Internet B");
-    p.devices
-      .get(p.lastId)
-      ?.internalState.netInterfaces.push({
-        name: "se2",
-        maxMbps: 100,
-        type: "serial",
-        mac: 0x102030405060,
-      });
+    p.devices.get(p.lastId)?.internalState.netInterfaces.push({
+      name: "se2",
+      maxMbps: 100,
+      type: "serial",
+      mac: 0x102030405060,
+    });
     (p.devices.get(p.lastId)?.internalState as RouterInternalState).l3Ifs[3] = {
       ip: parseIpv4("1.1.1.2")!,
       mask: parseIpv4("255.255.255.0")!,
