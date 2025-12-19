053a5441694d428c5fb386b87ecf7033422cb97a
amedeo <amedeo.alfonsi@yahoo.com>
2025-12-19 22:12:19 +0100

    Make ConnectTool UI working, first draft

--- app/tools/ConnectTool.tsx
+++ app/tools/ConnectTool.tsx
@@ -3,6 +3,7 @@ import { Tool, ToolCtx } from "./Tool";
 import { Coords } from "../common";
 import { intfColor } from "../editorComponents/Cables";
 import { toInterfaceId } from "../ProjectManager";
+import { ReactNode } from "react";
 
 export type ConnectTool = Tool & {
   deviceA?: Device;
@@ -29,56 +30,26 @@ export function makeConnectTool(ctx: ToolCtx): ConnectTool {
     ...ctx,
     toolname: "connect",
     panel() {
-      switch (true) {
-        case !this.deviceA:
-          return <>Seleziona un dispositivo</>;
-        case this.idxA === undefined:
-          return <>Seleziona un interfaccia (TODO)</>;
-        case !this.deviceB:
-          return <>Seleziona il secondo dispositivo</>;
-        default:
-          return <div className="flex flex-wrap indent-0">
-
-                  <div className="p-[10px] w-[50%]">
-                  <textarea className="resize-none rounded-md bg-white h-6 w-full mb-[10px]"></textarea>
-
-                    <div className="flex items-center justify-between">
-                      <div className="bg-white h-6 w-17 rounded-md"></div>
-                      <label className="relative inline-flex items-center cursor-pointer">
-                        <input type="checkbox" className="sr-only peer"></input>
-                        <div className="w-16 h-8 bg-red-500 rounded-full peer-checked:bg-green-500"></div>
-                        <div className="absolute left-1 top-1 w-6 h-6 bg-white rounded-full transition peer-checked:translate-x-8"></div>
-                      </label>
-                    </div>
-
-                    <div className="flex justify-center items-center gap-6 pt-6">
-                      <span className="text-white text-2xl cursor-pointer">&lt;</span>
-                      <div className="bg-white rounded-md w-10 h-6"></div>
-                      <span className="text-white text-2xl cursor-pointer">&gt;</span>
-                    </div>
-                  </div>
-
-                  <div className="p-[10px] w-[50%]">
-                  <textarea className="resize-none rounded-md bg-white h-6 w-full mb-[10px]"></textarea>
-
-                    <div className="flex items-center justify-between">
-                      <div className="bg-white h-6 w-17 rounded-md"></div>
-                      <label className="relative inline-flex items-center cursor-pointer">
-                        <input type="checkbox" className="sr-only peer"></input>
-                        <div className="w-16 h-8 bg-red-500 rounded-full peer-checked:bg-green-500"></div>
-                        <div className="absolute left-1 top-1 w-6 h-6 bg-white rounded-full transition peer-checked:translate-x-8"></div>
-                      </label>
-                    </div>
-
-                    <div className="flex justify-center items-center gap-6 pt-6">
-                      <span className="text-white text-2xl cursor-pointer">&lt;</span>
-                      <div className="bg-white rounded-md w-10 h-6"></div>
-                      <span className="text-white text-2xl cursor-pointer">&gt;</span>
-                    </div>
-                  </div>
-                  
-                </div>;
-      }
+      return (
+        <div className="flex flex-wrap indent-0">
+          {!this.deviceA ? (
+            <>Seleziona il primo dispositivo</>
+          ) : (
+            <>
+              <InterfaceSelector
+                device={this.deviceA}
+                intfIdx={this.idxA}
+                connectTool={this}
+              />
+              {!this.deviceB ? (
+                <>Seleziona il secondo dispositivo</>
+              ) : (
+                <InterfaceSelector device={this.deviceB} connectTool={this} />
+              )}
+            </>
+          )}
+        </div>
+      );
     },
     onEvent(ev) {
       const firstEmptyInterface = (device: Device): number => {
@@ -140,4 +111,77 @@ export function makeConnectTool(ctx: ToolCtx): ConnectTool {
       return <></>;
     },
   };
+}
+
+function Btn({
+  onClick,
+  className: extraClass,
+  children,
+}: {
+  onClick: () => void;
+  className: string;
+  children: ReactNode;
+}) {
+  return (
+    <button
+      className={"h-8 w-24 px-3 rounded-md font-bold " + extraClass}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
+
+function InterfaceSelector({
+  device,
+  intfIdx,
+  connectTool,
+}: {
+  device: Device;
+  intfIdx?: number;
+  connectTool: Pick<ConnectTool, "project" | "updateProject">;
+}) {
+  const isConnected = (i: number) =>
+    connectTool.project.getConnectedTo(toInterfaceId(device.id, i)) !==
+    undefined;
+  return (
+    <div className="p-[10px] w-[50%] text-black">
+      <textarea
+        className="resize-none rounded-md bg-white h-6 w-full mb-[10px]"
+        value={device.name}
+      ></textarea>
+
+      {device.internalState.netInterfaces.map((intf, i) => (
+        <div key={i} className="flex items-center justify-between m-1">
+          <div className="bg-white h-6 w-17 rounded-md">{intf.name}</div>
+          {/*
+        <label className="relative inline-flex items-center cursor-pointer">
+          <input type="checkbox" className="sr-only peer"></input>
+          <div className="w-16 h-8 bg-red-500 rounded-full peer-checked:bg-green-500"></div>
+          <div className="absolute left-1 top-1 w-6 h-6 bg-white rounded-full transition peer-checked:translate-x-8"></div>
+        </label>
+        */}
+          {i === intfIdx ? (
+            <Btn onClick={() => {}} className="text-red-900 bg-red-400">
+              Selezionata
+            </Btn>
+          ) : isConnected(i) ? (
+            <Btn onClick={() => {}} className="text-red-900 bg-red-400">
+              Scollega
+            </Btn>
+          ) : (
+            <Btn onClick={() => {}} className="text-slate-900 bg-slate-400">
+              Seleziona
+            </Btn>
+          )}
+        </div>
+      ))}
+
+      {/* <div className="flex justify-center items-center gap-6 pt-6"> */}
+      {/*   <span className="text-white text-2xl cursor-pointer">&lt;</span> */}
+      {/*   <div className="bg-white rounded-md w-10 h-6"></div> */}
+      {/*   <span className="text-white text-2xl cursor-pointer">&gt;</span> */}
+      {/* </div> */}
+    </div>
+  );
 }
