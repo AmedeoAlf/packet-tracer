3a1f3515ce14dc917e9dcb566183c2cdd69efba9
amedeo <amedeo.alfonsi@yahoo.com>
2026-02-06 16:36:20 +0100

    Improve LabelTool.tsx

--- app/tools/LabelTool.tsx
+++ app/tools/LabelTool.tsx
@@ -13,30 +13,33 @@ export function makeLabelTool(prev: LabelTool | object = {}): LabelTool {
     ...prev,
     toolname: "label",
     panel: (ctx) => {
-      if (!ctx.tool.currInput)
-        return (
-          <p className="h-8 rounded-md font-bold m-2 px-2 p-1 bg-gray-800 text-gray-500 text-center">
-            Clicca sullo scenario per aggiungere testo
-          </p>
-        );
+      if (!ctx.tool.currInput) return;
       return (
-        <>
-          <p className="h-8 rounded-md font-bold m-2 px-2 p-1 bg-gray-800 text-gray-500 text-center">
-            Clicca sullo scenario per aggiungere testo
-          </p>
-          <div className="h-10 rounded-md font-bold m-2 px-2 p-2 bg-gray-700 text-gray-400 text-center w-[60%]">
-            Contenuto:
-            <input
-              type="text"
+        <div className="w-full p-2">
+          <div className="rounded-md font-bold px-2 p-2 bg-gray-700 text-gray-400 text-center w-full">
+            Contenuto: <br />
+            <textarea
+              className="w-full"
               value={ctx.tool.currInput.text}
               onChange={(ev) => {
                 ctx.tool.currInput!.text = ev.target.value;
                 ctx.updateTool();
               }}
+              onKeyDown={(ev) => {
+                if (ev.key == "Enter") {
+                  ctx.project.addDecal({
+                    type: "text",
+                    ...ctx.tool.currInput!,
+                  });
+                  ctx.updateProject();
+                  ctx.tool.currInput = undefined;
+                  ctx.updateTool();
+                }
+              }}
               autoFocus
             />
           </div>
-        </>
+        </div>
       );
     },
     onEvent: (ctx, ev) => {
