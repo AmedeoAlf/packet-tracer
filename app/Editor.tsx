1da39e98964f41e2c6d95b30afac5e47c89d6b91
Amedeo Alfonsi <amedeo.alfonsi@gmail.com>
2026-03-03 16:04:57 +0100

    Move Decals.tsx out of Editor.tsx

--- app/Editor.tsx
+++ app/Editor.tsx
@@ -24,6 +24,7 @@ import { ProjectManager } from "./ProjectManager";
 import { Decal } from "./Project";
 import { TopBarBtns } from "./editorComponents/TopBarBtns";
 import { Coords } from "./common";
+import { Decals } from "./editorComponents/Decals";
 
 /*
  * Questo componente è tutta l'interfaccia del sito. Crea gli hook sia per il
@@ -305,51 +306,3 @@ function buildMouseEventHandler(
     };
   }
 }
-
-const Decals = memo(
-  function Decals({
-    decals,
-    tool,
-  }: {
-    decals: (Decal | null)[];
-    tool: Tool<object> | SelectTool;
-  }): ReactNode {
-    const highlighted = isSelectTool(tool)
-      ? isDecalHighlighted.bind(null, tool)
-      : undefined;
-    return decals.map((d, idx) => {
-      if (!d) return;
-      const data = { "data-decalid": idx };
-      switch (d.type) {
-        case "text":
-          return (
-            <text
-              key={idx}
-              x={d.pos[0]}
-              y={d.pos[1]}
-              {...data}
-              fill={d.fg}
-              className={highlighted && highlighted(d) ? "opacity-50" : ""}
-            >
-              {d.text}
-            </text>
-          );
-        case "rect":
-          return (
-            <rect
-              key={idx}
-              x={d.pos[0]}
-              y={d.pos[1]}
-              {...d.size}
-              stroke={d.stroke ?? "none"}
-              fill={d.fill ?? "none"}
-              {...data}
-            />
-          );
-      }
-    });
-  },
-  (p, n) =>
-    p.decals == n.decals &&
-    (p.tool == n.tool || (!isSelectTool(p.tool) && !isSelectTool(n.tool))),
-);
---
+++ app/editorComponents/Decals.tsx
@@ -0,0 +1,52 @@
+import { memo, ReactNode } from "react";
+import { Decal } from "../Project";
+import { Tool } from "../tools/Tool";
+import { isDecalHighlighted, isSelectTool, SelectTool } from "../tools/SelectTool";
+
+export const Decals = memo(
+  function Decals({
+    decals,
+    tool,
+  }: {
+    decals: (Decal | null)[];
+    tool: Tool<object> | SelectTool;
+  }): ReactNode {
+    const highlighted = isSelectTool(tool)
+      ? isDecalHighlighted.bind(null, tool)
+      : undefined;
+    return decals.map((d, idx) => {
+      if (!d) return;
+      const data = { "data-decalid": idx };
+      switch (d.type) {
+        case "text":
+          return (
+            <text
+              key={idx}
+              x={d.pos[0]}
+              y={d.pos[1]}
+              {...data}
+              fill={d.fg}
+              className={highlighted && highlighted(d) ? "opacity-50" : ""}
+            >
+              {d.text}
+            </text>
+          );
+        case "rect":
+          return (
+            <rect
+              key={idx}
+              x={d.pos[0]}
+              y={d.pos[1]}
+              {...d.size}
+              stroke={d.stroke ?? "none"}
+              fill={d.fill ?? "none"}
+              {...data}
+            />
+          );
+      }
+    });
+  },
+  (p, n) =>
+    p.decals == n.decals &&
+    (p.tool == n.tool || (!isSelectTool(p.tool) && !isSelectTool(n.tool))),
+);
