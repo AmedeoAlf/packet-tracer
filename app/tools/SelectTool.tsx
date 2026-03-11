884a1ae1bd60043c1bdd934bfa9eaa8a58fa1440
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2026-03-11 18:30:27 +0100

    Stop react-doctor from complaining about components

--- app/tools/SelectTool.tsx
+++ app/tools/SelectTool.tsx
@@ -534,6 +534,46 @@ function duplicateSelection(self: SelectTool, project: ProjectManager) {
   self.selectedDecals = newDecals;
 }
 
+function Square(props: { size: number; x: number; y: number }) {
+  const { size, ...other } = props;
+  return (
+    <rect
+      width={size}
+      height={size}
+      rx={1}
+      ry={1}
+      stroke="white"
+      fill="none"
+      {...other}
+    />
+  );
+}
+
+function Path(props: { d: string }) {
+  return (
+    <path
+      stroke="white"
+      fill="none"
+      strokeLinejoin="round"
+      strokeLinecap="round"
+      {...props}
+    />
+  );
+}
+
+function VLine({ x, from, to }: { x: number; from: number; to: number }) {
+  return (
+    <line
+      x1={x}
+      x2={x}
+      y1={from}
+      y2={to}
+      stroke="white"
+      strokeLinecap="round"
+    />
+  );
+}
+
 function SelectionActions({
   duplicate,
   del,
@@ -546,53 +586,22 @@ function SelectionActions({
   children: ReactNode;
 }) {
   const CLASSNAME = "bg-zinc-700 flex-1";
-  const Rect = (x: number, y: number) => (
-    <rect
-      x={x}
-      y={y}
-      width={6}
-      height={6}
-      rx={1}
-      ry={1}
-      stroke="white"
-      fill="none"
-    />
-  );
-  const Path = (d: string) => (
-    <path
-      d={d}
-      stroke="white"
-      fill="none"
-      strokeLinejoin="round"
-      strokeLinecap="round"
-    />
-  );
-  const VLine = (x: number, from: number, to: number) => (
-    <line
-      x1={x}
-      x2={x}
-      y1={from}
-      y2={to}
-      stroke="white"
-      strokeLinecap="round"
-    />
-  );
   return (
     <div className={"flex w-full items-center " + (className ?? "")}>
       {children}
       <BtnArray>
         <BtnArrEl className={CLASSNAME} onClick={duplicate}>
           <svg width={20} height={20} viewBox="0 0 10 10">
-            {Rect(1, 1)}
-            {Rect(3, 3)}
+            <Square size={6} x={1} y={1} />
+            <Square size={6} x={3} y={3} />
           </svg>
         </BtnArrEl>
         <BtnArrEl className={CLASSNAME} onClick={del}>
           <svg width={20} height={20} viewBox="0 0 12 12">
-            {Path("M 2.5 1.5 H 6 v -0.5 v 0.5 H 9.5")}
-            {VLine(7, 4.6, 9.4)}
-            {VLine(5, 4.6, 9.4)}
-            {Path("M 2 3 l 1 1 V 10 q 0,1 1,1 H 8 q 1,0 1,-1 V 4 l 1 -1 z")}
+            <Path d="M 2.5 1.5 H 6 v -0.5 v 0.5 H 9.5" />
+            <VLine x={7} from={4.6} to={9.4} />
+            <VLine x={5} from={4.6} to={9.4} />
+            <Path d="M 2 3 l 1 1 V 10 q 0,1 1,1 H 8 q 1,0 1,-1 V 4 l 1 -1 z" />
           </svg>
         </BtnArrEl>
       </BtnArray>
