8a5155da901cab96e24e2b9b02eca3fe6fa905ea
amedeo <amedeo.alfonsi@yahoo.com>
2026-04-14 22:28:00 +0200

    Resolve addresses on curl

--- app/virtualPrograms/curl.tsx
+++ app/virtualPrograms/curl.tsx
@@ -1,8 +1,9 @@
 import { OSInternalState } from "../devices/list/Computer";
-import { SubCommand } from "../emulators/DeviceEmulator";
+import { EmulatorContext, SubCommand } from "../emulators/DeviceEmulator";
+import { getDns, resolveAddressSimple } from "../emulators/utils/dnsUtils";
 import { send, recv, close, dialTCP } from "../emulators/utils/sockets";
 import { HttpRequest, HttpResponse } from "../protocols/http";
-import { parseIpv4 } from "../protocols/rfc_760";
+import { IPv4Address, parseIpv4 } from "../protocols/rfc_760";
 
 export const curl = {
   desc: "Gets the content of a resource through http",
@@ -15,25 +16,48 @@ export const curl = {
   then: {
     done: true,
     run(ctx) {
-      const [address, resource] = decodeURI(ctx.args![1]).split("/");
-      // TODO: resolve domain names and send Host param
-      const socket = dialTCP(ctx, parseIpv4(address)!, 80, (ctx, socket) => {
-        send(
-          ctx,
-          socket,
-          new HttpRequest("/" + resource, {
-            "user-agent": "curl",
-          }).toBytes(),
-        );
-        recv(ctx.state, socket, (ctx, data) => {
-          const response = HttpResponse.fromBytes(data);
-          console.log("server answered", response);
-          if (!(response instanceof HttpResponse)) return;
-          ctx.write(response.body.toString());
-          close(ctx, socket);
-        });
+      const [address, resource] = encodeURI(ctx.args![1]).split("/");
+      const ip = parseIpv4(address);
+
+      // The user passed an IP as destination, no DNS needed
+      if (typeof ip !== 'undefined') {
+        curlRequest(ctx, ip, resource);
+        return;
+      }
+
+      const dns = getDns(ctx);
+      if (typeof dns == 'undefined') {
+        ctx.write("Configure dns to resolve server addresses (write the ip in /etc/dns)");
+        return;
+      }
+      resolveAddressSimple(ctx, dns, address, (ctx, ip, error) => {
+        if (typeof error == "string") {
+          ctx.write("ERROR: " + error);
+          return;
+        }
+        curlRequest(ctx, ip, resource, address);
       });
-      ctx.schedule(50, (ctx) => close(ctx, socket));
     },
   },
 } satisfies SubCommand<OSInternalState>;
+
+function curlRequest(ctx: EmulatorContext<OSInternalState>, ip: IPv4Address, resource: string, domain?: string) {
+  const socket = dialTCP(ctx, ip, 80, (ctx, socket) => {
+    send(
+      ctx,
+      socket,
+      new HttpRequest("/" + resource, {
+        "user-agent": "curl",
+        "host": domain
+      }).toBytes(),
+    );
+    recv(ctx.state, socket, (ctx, data) => {
+      const response = HttpResponse.fromBytes(data);
+      console.log("server answered", response);
+      if (!(response instanceof HttpResponse)) return;
+      ctx.write(response.body.toString());
+      close(ctx, socket);
+    });
+  });
+  ctx.schedule(50, (ctx) => close(ctx, socket));
+}
