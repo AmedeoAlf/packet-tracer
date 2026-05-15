213d7297b95d4042eafd84827383d343fde8a6e4
amedeo <amedeo.alfonsi@yahoo.com>
2026-05-15 21:44:36 +0200

    Make className of TextInput customizable

--- app/editorComponents/TextInput.tsx
+++ app/editorComponents/TextInput.tsx
@@ -1,13 +1,18 @@
 export function TextInput({
   value,
   setValue,
+  className = "w-full text-xl",
 }: {
   value: string;
   setValue: (s: string) => void;
+  className?: string;
 }) {
   return (
     <input
-      className="text-xl font-bold flex-1 bg-onsidebar w-full px-2 py-1 rounded-md border-b"
+      className={
+        "font-bold flex-1 bg-onsidebar px-2 py-1 rounded-md border-b " +
+        className
+      }
       type="text"
       size={2}
       value={value}
