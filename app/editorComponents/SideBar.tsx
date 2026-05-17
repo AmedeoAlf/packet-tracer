fc9828c05882106b00a5094572fbf5f7e3ab41ca
AmedeoAlf <amedeo.alfonsi@yahoo.com>
2026-05-17 22:12:21 +0200

    true mirror in Sidebar.tsx

--- app/editorComponents/SideBar.tsx
+++ app/editorComponents/SideBar.tsx
@@ -1,4 +1,4 @@
-import { memo, ReactNode, useEffect, useState } from "react";
+import { memo, ReactNode, useEffect, useRef, useState } from "react";
 import { ToolCtx } from "../tools/Tool";
 
 // La barra laterale dell'interfaccia: il suo contenuto è intermente deciso dal
@@ -14,7 +14,7 @@ export function SideBar({
   initialWidth?: number;
   minWidth?: number;
 }) {
-  const resizeBarId = "sidebarResizeHandle" + (rightSide ? "R" : "L");
+  const resizeBarRef = useRef<HTMLDivElement>(null);
   const [open, setOpen] = useState(true);
   const [resizing, setResizing] = useState(false);
   const [width, setWidth] = useState(initialWidth);
@@ -43,18 +43,18 @@ export function SideBar({
   useEffect(() => {
     const mousedown = (ev: MouseEvent) => {
       if (!(ev.target instanceof HTMLDivElement)) return;
-      if (ev.target.id != resizeBarId) return;
+      if (ev.target != resizeBarRef.current) return;
       setResizing(true);
     };
     document.addEventListener("mousedown", mousedown);
     return () => document.removeEventListener("mousedown", mousedown);
-  }, [setResizing, resizeBarId]);
+  }, [setResizing]);
 
   return (
     <>
       <div
         className={
-          `transition fixed top-12.5 h-(--h-spec-cont) ${rightSide ? "right" : "left"}-0 p-3 pointer-events-none ` +
+          `transition fixed top-12.5 h-(--h-spec-cont) ${rightSide ? "right-0" : "left-0"} p-3 pointer-events-none ` +
           (children && open
             ? ""
             : rightSide
@@ -65,10 +65,10 @@ export function SideBar({
       >
         <div className="bg-sidebar p-4 border-zinc-500 border-2 w-full pl-5 rounded-xl pointer-events-auto overflow-y-auto h-max max-h-full relative">
           <div
-            className={`absolute top-0 ${rightSide ? "left" : "right"}-0 h-full py-4`}
+            className={`absolute top-0 ${rightSide ? "left-0" : "right-0"} h-full py-4`}
           >
             <div
-              id={resizeBarId}
+              ref={resizeBarRef}
               className="w-3 z-1 h-full select-none box-border flex items-center justify-center group"
             >
               <div
@@ -84,9 +84,10 @@ export function SideBar({
       </div>
       <button
         className={
-          `transition fixed top-12.5 right-0 w-6 h-(--h-spec-cont)
+          `transition fixed top-12.5 ${rightSide ? "right-0" : "-scale-x-100 left-0"} w-6 h-(--h-spec-cont)
             bg-radial-[at_100%_50%] from-sky-700 to-transparent to-60%
-            hover:to-80% ` + (children ? "" : "translate-x-10")
+            hover:to-80% ` +
+          (children ? "" : rightSide ? "translate-x-10" : "-translate-x-10")
         }
         onClick={() => setOpen(!open)}
       >
