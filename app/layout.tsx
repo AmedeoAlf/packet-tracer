0218b0085d4ecdf59e8de4089aa82001d88e7384
amedeo <amedeo.alfonsi@yahoo.com>
2026-08-20 11:58:39 +0200

    finally remove the suppress hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         {process.env.NODE_ENV == "development" && (
           <Script
